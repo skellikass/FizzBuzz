44fea9a68cddbd75c4a15a25e7f673b83a45917f
Kassandra Johnson <kassandra.m.johnson@gmail.com>
2022-12-29 12:32:50 -0500

    inital commit, completed exercise

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzClass
+    {
+        public static string FizzBuzzMethod(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "fizzbuzz";
+            }
+            if (number % 3 == 0)
+            {
+                return "fizz";
+            }
+            if (number % 5 == 0)
+            {
+                return "buzz";
+            }
+            else return "";
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            Console.WriteLine(FizzBuzzClass.FizzBuzzMethod(15));
+            Console.WriteLine(FizzBuzzClass.FizzBuzzMethod(9));
+            Console.WriteLine(FizzBuzzClass.FizzBuzzMethod(10));
+        }
+    }
+}
